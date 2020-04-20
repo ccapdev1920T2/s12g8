2549c6f55dc6cb66c172c19e9927599d4b4121b4
Andilyn <57976631+Andilyn@users.noreply.github.com>
2020-04-20 21:23:50 +0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -8,7 +8,7 @@ mongoose.set('useFindAndModify', false);
 const database = {
 
     connect: function () {
-        mongoose.connect(`mongodb+srv://carlos:${password}@mpdatabase-l4c9f.mongodb.net/test?retryWrites=true&w=majority`, { 
+        mongoose.connect(`mongodb://carlos:p%40ssword@mpdatabase-shard-00-00-l4c9f.mongodb.net:27017,mpdatabase-shard-00-01-l4c9f.mongodb.net:27017,mpdatabase-shard-00-02-l4c9f.mongodb.net:27017/test?ssl=true&replicaSet=MPDatabase-shard-0&authSource=admin&retryWrites=true&w=majority`, { 
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
